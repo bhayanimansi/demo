3fa2bc7fa6fdea28f03b46c19756c099f07d6256
bhayanimansi <141470338+bhayanimansi@users.noreply.github.com>
2023-08-05 09:47:07 +0530

    Add files via upload

---
+++ cprogram.py
@@ -0,0 +1,18 @@
+#program to print creative or multimedia  
+a=25
+b=10
+c=5
+
+if a>10 and b>10 and c>10:
+    print("creative")
+else:
+    print("multimedia")
+
+
+
+if a>10:
+    if b>10:
+        if c>10:
+            print("creative")
+else:
+            print("multimedia")
